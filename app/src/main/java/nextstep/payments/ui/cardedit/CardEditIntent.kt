99fd4bed66ccc6529a489174f59058b876abf72e
ver <ver@sendy.ai>
2025-03-10 08:54:34 +0900

    feat: 카드를 수정할 수 있다

--- app/src/main/java/nextstep/payments/ui/cardedit/CardEditIntent.kt
+++ app/src/main/java/nextstep/payments/ui/cardedit/CardEditIntent.kt
@@ -1,5 +1,13 @@
 package nextstep.payments.ui.cardedit
 
+import nextstep.payments.model.IssuingBank
+
 sealed interface CardEditIntent {
     data class FetchCreditCard(val cardId: Long) : CardEditIntent
+    data class OnCardNumberChanged(val cardNumber: String) : CardEditIntent
+    data class OnExpiredDateChanged(val expiredDate: String) : CardEditIntent
+    data class OnOwnerNameChanged(val ownerName: String) : CardEditIntent
+    data class OnPasswordChanged(val password: String) : CardEditIntent
+    data class OnIssuingBankChanged(val issuingBank: IssuingBank) : CardEditIntent
+    data object OnSaveCardEdit : CardEditIntent
 }
--- app/src/main/java/nextstep/payments/ui/cardedit/CardEditViewModel.kt
+++ app/src/main/java/nextstep/payments/ui/cardedit/CardEditViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import nextstep.payments.model.IssuingBank
 import nextstep.payments.repository.PaymentCardsRepository
 
 class CardEditViewModel(
@@ -24,6 +25,12 @@ class CardEditViewModel(
     fun onIntent(intent: CardEditIntent) {
         when (intent) {
             is CardEditIntent.FetchCreditCard -> fetchCreditCard(intent.cardId)
+            is CardEditIntent.OnCardNumberChanged -> changeCardNumber(intent.cardNumber)
+            is CardEditIntent.OnExpiredDateChanged -> changeExpireDate(intent.expiredDate)
+            is CardEditIntent.OnOwnerNameChanged -> changeOwnerName(intent.ownerName)
+            is CardEditIntent.OnPasswordChanged -> changePassword(intent.password)
+            is CardEditIntent.OnIssuingBankChanged -> changeIssuingBank(intent.issuingBank)
+            is CardEditIntent.OnSaveCardEdit -> saveCardEdit()
         }
     }
 
@@ -35,6 +42,50 @@ class CardEditViewModel(
                 return@launch
             }
             _uiState.value = CardEditUiState.Success(card)
+        }
+    }
+
+    private fun changeCardNumber(cardNumber: String) = setStateOnSuccess {
+        copy(creditCard = creditCard.copy(cardNumber = cardNumber))
+    }
+
+    private fun changeExpireDate(expiredDate: String) = setStateOnSuccess {
+        copy(creditCard = creditCard.copy(expiredDate = expiredDate))
+    }
+
+    private fun changeOwnerName(ownerName: String) = setStateOnSuccess {
+        copy(creditCard = creditCard.copy(ownerName = ownerName))
+    }
+
+    private fun changePassword(password: String) = setStateOnSuccess {
+        copy(creditCard = creditCard.copy(password = password))
+    }
+
+    private fun changeIssuingBank(issuingBank: IssuingBank) = setStateOnSuccess {
+        copy(creditCard = creditCard.copy(issuingBank = issuingBank))
+    }
+
+    private fun setStateOnSuccess(
+        reducer: CardEditUiState.Success.() -> CardEditUiState.Success
+    ) {
+        val state = _uiState.value as? CardEditUiState.Success ?: return
+        _uiState.value = reducer(state)
+    }
+
+    private fun saveCardEdit() {
+        viewModelScope.launch {
+            val success = _uiState.value as? CardEditUiState.Success ?: return@launch
+            val lastCard = repository.findCard(success.creditCard.id)
+            if (lastCard == null) {
+                _effect.send(CardEditEffect.ShowError("수정하려는 카드 정보를 찾을 수 없습니다."))
+                return@launch
+            }
+            if (lastCard == success.creditCard) {
+                _effect.send(CardEditEffect.ShowError("변경된 내용이 없습니다."))
+                return@launch
+            }
+            repository.updateCard(success.creditCard)
+            _effect.send(CardEditEffect.OnCardEditSaved)
         }
     }
 }
